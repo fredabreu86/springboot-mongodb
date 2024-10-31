8468e5da780766d44e21a72ca1717c0b016ec32f
Fred Abreu <fredabreu86@gmail.com>
2024-10-31 15:06:23 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projetos/springbootmongo/domain/User.java
+++ src/main/java/com/projetos/springbootmongo/domain/User.java
@@ -3,10 +3,20 @@ package com.projetos.springbootmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String id;
 	private String name;
 	private String email;
@@ -61,4 +71,6 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 }
