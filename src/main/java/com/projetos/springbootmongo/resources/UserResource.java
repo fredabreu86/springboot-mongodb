44baf900103b75504728f476ed98d10ba6327240
Fred Abreu <fredabreu86@gmail.com>
2024-11-04 10:48:35 -0300

    Service layer, component registration

--- src/main/java/com/projetos/springbootmongo/resources/UserResource.java
+++ src/main/java/com/projetos/springbootmongo/resources/UserResource.java
@@ -1,26 +1,35 @@
 package com.projetos.springbootmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetos.springbootmongo.domain.User;
+import com.projetos.springbootmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@RequestMapping(method=RequestMethod.GET)
+	@Autowired
+	private UserService service;
+
+	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User fred = new User(1L, "Fred", "fredabreu@gmail.com", "61998039666", "12345");
-		User lud = new User(2L, "Lud", "lud@gmail.com", "61998039666", "12345");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(fred, lud));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+
 }
