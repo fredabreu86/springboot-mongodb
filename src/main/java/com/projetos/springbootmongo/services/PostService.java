8786976d0133b15fbe675da7dc40f61783fbc0b8
Fred Abreu <fredabreu86@gmail.com>
2025-01-16 11:53:33 -0300

    Consulta com vários critérios

--- src/main/java/com/projetos/springbootmongo/services/PostService.java
+++ src/main/java/com/projetos/springbootmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.projetos.springbootmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repository.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
